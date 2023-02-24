6f693120d1e2d9f9a0c8ee7a03dc9054a2904feb
Choromi Tao <princessailish94@mail.ru>
2023-02-24 15:51:52 +0900

    lession from 21/02

---
+++ Lession_02.21.23/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+
+int GetNumber()
+{
+    Console.WriteLine("Input number:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Numbers()
+{
+    int a = GetNumber();
+    int count = 0;
+    while (a >0)
+    {
+        a = a / 10;
+        count ++;
+    }
+    return count;
+}
+
+int Result()
+{
+    int b = GetNumber();
+    int x = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        x = x*i;
+    }
+    return x;
+}
+
+Console.WriteLine(Result());
---
+++ Lession_02.21.23/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Random number = new Random();
+int[] arr = new int [8];
+for (int i = 0; i < arr.length; i++)
+{
+    arr[i] = random.Next(0,2);
+    string array = String.Join("   ", arr);
+    Console.WriteLine(array);
+}
